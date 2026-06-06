90637cb3ffb1db226b6ec2bce5eedf5bcfe10e84
fredcypher <chessnthisgalaxy@gmail.com>
2026-06-06 14:40:45 +0900

    Add files via upload

---
+++ cypherFlippingMatrix.js
@@ -0,0 +1,896 @@
+var cypherFlippingMatrix = {
+  "Ck:z0": [
+    "Fs:a1"
+  ],
+  "Ck:z1": [
+    "Fs:a1",
+    "Fs:a2"
+  ],
+  "Ck:z2": [
+    "Fs:a1",
+    "Fs:a2",
+    "Fs:a3"
+  ],
+  "Ck:z3": [
+    "Fs:a2",
+    "Fs:a3",
+    "Fs:a4"
+  ],
+  "Ck:z4": [
+    "Fs:a3",
+    "Fs:a4",
+    "Bs:a5"
+  ],
+  "Ck:z5": [
+    "Fs:a4",
+    "Bs:a5",
+    "Fs:a6"
+  ],
+  "Ck:z6": [
+    "Bs:a5",
+    "Fs:a6",
+    "Fs:a7"
+  ],
+  "Ck:z7": [
+    "Fs:a6",
+    "Fs:a7",
+    "Fs:a8"
+  ],
+  "Ck:z8": [
+    "Fs:a7",
+    "Fs:a8",
+    "Fs:a9"
+  ],
+  "Ck:z9": [
+    "Fs:a8",
+    "Fs:a9"
+  ],
+  "Ck:z10": [
+    "Fs:a9"
+  ],
+  "Ck:a0": [
+    "Fs:a1",
+    "Fs:b1"
+  ],
+  "Fk:a1": [
+    "Cs:z0",
+    "Cs:z1",
+    "Cs:z2",
+    "Cs:a0",
+    "Fs:a2",
+    "Cs:b0",
+    "Fs:b1",
+    "Fs:b2"
+  ],
+  "Fk:a2": [
+    "Cs:z1",
+    "Cs:z2",
+    "Cs:z3",
+    "Fs:a1",
+    "Fs:a3",
+    "Fs:b1",
+    "Fs:b2",
+    "Fs:b3"
+  ],
+  "Fk:a3": [
+    "Cs:z2",
+    "Cs:z3",
+    "Cs:z4",
+    "Fs:a2",
+    "Fs:a4",
+    "Fs:b2",
+    "Fs:b3",
+    "Fs:b4"
+  ],
+  "Fk:a4": [
+    "Cs:z3",
+    "Cs:z4",
+    "Cs:z5",
+    "Fs:a3",
+    "Bs:a5",
+    "Fs:b3",
+    "Fs:b4",
+    "Bs:b5"
+  ],
+  "Bk:a5": [
+    "Cs:z4",
+    "Cs:z5",
+    "Cs:z6",
+    "Fs:a4",
+    "Fs:a6",
+    "Fs:b4",
+    "Bs:b5",
+    "Fs:b6"
+  ],
+  "Fk:a6": [
+    "Cs:z5",
+    "Cs:z6",
+    "Cs:z7",
+    "Bs:a5",
+    "Fs:a7",
+    "Bs:b5",
+    "Fs:b6",
+    "Fs:b7"
+  ],
+  "Fk:a7": [
+    "Cs:z6",
+    "Cs:z7",
+    "Cs:z8",
+    "Fs:a6",
+    "Fs:a8",
+    "Fs:b6",
+    "Fs:b7",
+    "Fs:b8"
+  ],
+  "Fk:a8": [
+    "Cs:z7",
+    "Cs:z8",
+    "Cs:z9",
+    "Fs:a7",
+    "Fs:a9",
+    "Fs:b7",
+    "Fs:b8",
+    "Fs:b9"
+  ],
+  "Fk:a9": [
+    "Cs:z8",
+    "Cs:z9",
+    "Cs:z10",
+    "Fs:a8",
+    "Cs:a10",
+    "Fs:b8",
+    "Fs:b9",
+    "Cs:b10"
+  ],
+  "Ck:a10": [
+    "Fs:a9",
+    "Fs:b9"
+  ],
+  "Ck:b0": [
+    "Fs:a1",
+    "Fs:b1",
+    "Fs:c1"
+  ],
+  "Fk:b1": [
+    "Cs:a0",
+    "Fs:a1",
+    "Fs:a2",
+    "Cs:b0",
+    "Fs:b2",
+    "Cs:c0",
+    "Fs:c1",
+    "Fs:c2"
+  ],
+  "Fk:b2": [
+    "Fs:a1",
+    "Fs:a2",
+    "Fs:a3",
+    "Fs:b1",
+    "Fs:b3",
+    "Fs:c1",
+    "Fs:c2",
+    "Fs:c3"
+  ],
+  "Fk:b3": [
+    "Fs:a2",
+    "Fs:a3",
+    "Fs:a4",
+    "Fs:b2",
+    "Fs:b4",
+    "Fs:c2",
+    "Fs:c3",
+    "Fs:c4"
+  ],
+  "Fk:b4": [
+    "Fs:a3",
+    "Fs:a4",
+    "Bs:a5",
+    "Fs:b3",
+    "Bs:b5",
+    "Fs:c3",
+    "Fs:c4",
+    "Bs:c5"
+  ],
+  "Bk:b5": [
+    "Fs:a4",
+    "Bs:a5",
+    "Fs:a6",
+    "Fs:b4",
+    "Fs:b6",
+    "Fs:c4",
+    "Bs:c5",
+    "Fs:c6"
+  ],
+  "Fk:b6": [
+    "Bs:a5",
+    "Fs:a6",
+    "Fs:a7",
+    "Bs:b5",
+    "Fs:b7",
+    "Bs:c5",
+    "Fs:c6",
+    "Fs:c7"
+  ],
+  "Fk:b7": [
+    "Fs:a6",
+    "Fs:a7",
+    "Fs:a8",
+    "Fs:b6",
+    "Fs:b8",
+    "Fs:c6",
+    "Fs:c7",
+    "Fs:c8"
+  ],
+  "Fk:b8": [
+    "Fs:a7",
+    "Fs:a8",
+    "Fs:a9",
+    "Fs:b7",
+    "Fs:b9",
+    "Fs:c7",
+    "Fs:c8",
+    "Fs:c9"
+  ],
+  "Fk:b9": [
+    "Fs:a8",
+    "Fs:a9",
+    "Cs:a10",
+    "Fs:b8",
+    "Cs:b10",
+    "Fs:c8",
+    "Fs:c9",
+    "Cs:c10"
+  ],
+  "Ck:b10": [
+    "Fs:a9",
+    "Fs:b9",
+    "Fs:c9"
+  ],
+  "Ck:c0": [
+    "Fs:b1",
+    "Fs:c1",
+    "Fs:d1"
+  ],
+  "Fk:c1": [
+    "Cs:b0",
+    "Fs:b1",
+    "Fs:b2",
+    "Cs:c0",
+    "Fs:c2",
+    "Cs:d0",
+    "Fs:d1",
+    "Fs:d2"
+  ],
+  "Fk:c2": [
+    "Fs:b1",
+    "Fs:b2",
+    "Fs:b3",
+    "Fs:c1",
+    "Fs:c3",
+    "Fs:d1",
+    "Fs:d2",
+    "Fs:d3"
+  ],
+  "Fk:c3": [
+    "Fs:b2",
+    "Fs:b3",
+    "Fs:b4",
+    "Fs:c2",
+    "Fs:c4",
+    "Fs:d2",
+    "Fs:d3",
+    "Fs:d4"
+  ],
+  "Fk:c4": [
+    "Fs:b3",
+    "Fs:b4",
+    "Bs:b5",
+    "Fs:c3",
+    "Bs:c5",
+    "Fs:d3",
+    "Fs:d4",
+    "Bs:d5"
+  ],
+  "Bk:c5": [
+    "Fs:b4",
+    "Bs:b5",
+    "Fs:b6",
+    "Fs:c4",
+    "Fs:c6",
+    "Fs:d4",
+    "Bs:d5",
+    "Fs:d6"
+  ],
+  "Fk:c6": [
+    "Bs:b5",
+    "Fs:b6",
+    "Fs:b7",
+    "Bs:c5",
+    "Fs:c7",
+    "Bs:d5",
+    "Fs:d6",
+    "Fs:d7"
+  ],
+  "Fk:c7": [
+    "Fs:b6",
+    "Fs:b7",
+    "Fs:b8",
+    "Fs:c6",
+    "Fs:c8",
+    "Fs:d6",
+    "Fs:d7",
+    "Fs:d8"
+  ],
+  "Fk:c8": [
+    "Fs:b7",
+    "Fs:b8",
+    "Fs:b9",
+    "Fs:c7",
+    "Fs:c9",
+    "Fs:d7",
+    "Fs:d8",
+    "Fs:d9"
+  ],
+  "Fk:c9": [
+    "Fs:b8",
+    "Fs:b9",
+    "Cs:b10",
+    "Fs:c8",
+    "Cs:c10",
+    "Fs:d8",
+    "Fs:d9",
+    "Cs:d10"
+  ],
+  "Ck:c10": [
+    "Fs:b9",
+    "Fs:c9",
+    "Fs:d9"
+  ],
+  "Ck:d0": [
+    "Fs:c1",
+    "Fs:d1",
+    "Fs:e1"
+  ],
+  "Fk:d1": [
+    "Cs:c0",
+    "Fs:c1",
+    "Fs:c2",
+    "Cs:d0",
+    "Fs:d2",
+    "Cs:e0",
+    "Fs:e1",
+    "Fs:e2"
+  ],
+  "Fk:d2": [
+    "Fs:c1",
+    "Fs:c2",
+    "Fs:c3",
+    "Fs:d1",
+    "Fs:d3",
+    "Fs:e1",
+    "Fs:e2",
+    "Fs:e3"
+  ],
+  "Fk:d3": [
+    "Fs:c2",
+    "Fs:c3",
+    "Fs:c4",
+    "Fs:d2",
+    "Fs:d4",
+    "Fs:e2",
+    "Fs:e3",
+    "Fs:e4"
+  ],
+  "Fk:d4": [
+    "Fs:c3",
+    "Fs:c4",
+    "Bs:c5",
+    "Fs:d3",
+    "Bs:d5",
+    "Fs:e3",
+    "Fs:e4",
+    "Bs:e5"
+  ],
+  "Bk:d5": [
+    "Fs:c4",
+    "Bs:c5",
+    "Fs:c6",
+    "Fs:d4",
+    "Fs:d6",
+    "Fs:e4",
+    "Bs:e5",
+    "Fs:e6"
+  ],
+  "Fk:d6": [
+    "Bs:c5",
+    "Fs:c6",
+    "Fs:c7",
+    "Bs:d5",
+    "Fs:d7",
+    "Bs:e5",
+    "Fs:e6",
+    "Fs:e7"
+  ],
+  "Fk:d7": [
+    "Fs:c6",
+    "Fs:c7",
+    "Fs:c8",
+    "Fs:d6",
+    "Fs:d8",
+    "Fs:e6",
+    "Fs:e7",
+    "Fs:e8"
+  ],
+  "Fk:d8": [
+    "Fs:c7",
+    "Fs:c8",
+    "Fs:c9",
+    "Fs:d7",
+    "Fs:d9",
+    "Fs:e7",
+    "Fs:e8",
+    "Fs:e9"
+  ],
+  "Fk:d9": [
+    "Fs:c8",
+    "Fs:c9",
+    "Cs:c10",
+    "Fs:d8",
+    "Cs:d10",
+    "Fs:e8",
+    "Fs:e9",
+    "Cs:e10"
+  ],
+  "Ck:d10": [
+    "Fs:c9",
+    "Fs:d9",
+    "Fs:e9"
+  ],
+  "Ck:e0": [
+    "Fs:d1",
+    "Fs:e1",
+    "Fs:f1"
+  ],
+  "Fk:e1": [
+    "Cs:d0",
+    "Fs:d1",
+    "Fs:d2",
+    "Cs:e0",
+    "Fs:e2",
+    "Cs:f0",
+    "Fs:f1",
+    "Fs:f2"
+  ],
+  "Fk:e2": [
+    "Fs:d1",
+    "Fs:d2",
+    "Fs:d3",
+    "Fs:e1",
+    "Fs:e3",
+    "Fs:f1",
+    "Fs:f2",
+    "Fs:f3"
+  ],
+  "Fk:e3": [
+    "Fs:d2",
+    "Fs:d3",
+    "Fs:d4",
+    "Fs:e2",
+    "Fs:e4",
+    "Fs:f2",
+    "Fs:f3",
+    "Fs:f4"
+  ],
+  "Fk:e4": [
+    "Fs:d3",
+    "Fs:d4",
+    "Bs:d5",
+    "Fs:e3",
+    "Bs:e5",
+    "Fs:f3",
+    "Fs:f4",
+    "Bs:f5"
+  ],
+  "Bk:e5": [
+    "Fs:d4",
+    "Bs:d5",
+    "Fs:d6",
+    "Fs:e4",
+    "Fs:e6",
+    "Fs:f4",
+    "Bs:f5",
+    "Fs:f6"
+  ],
+  "Fk:e6": [
+    "Bs:d5",
+    "Fs:d6",
+    "Fs:d7",
+    "Bs:e5",
+    "Fs:e7",
+    "Bs:f5",
+    "Fs:f6",
+    "Fs:f7"
+  ],
+  "Fk:e7": [
+    "Fs:d6",
+    "Fs:d7",
+    "Fs:d8",
+    "Fs:e6",
+    "Fs:e8",
+    "Fs:f6",
+    "Fs:f7",
+    "Fs:f8"
+  ],
+  "Fk:e8": [
+    "Fs:d7",
+    "Fs:d8",
+    "Fs:d9",
+    "Fs:e7",
+    "Fs:e9",
+    "Fs:f7",
+    "Fs:f8",
+    "Fs:f9"
+  ],
+  "Fk:e9": [
+    "Fs:d8",
+    "Fs:d9",
+    "Cs:d10",
+    "Fs:e8",
+    "Cs:e10",
+    "Fs:f8",
+    "Fs:f9",
+    "Cs:f10"
+  ],
+  "Ck:e10": [
+    "Fs:d9",
+    "Fs:e9",
+    "Fs:f9"
+  ],
+  "Ck:f0": [
+    "Fs:e1",
+    "Fs:f1",
+    "Fs:g1"
+  ],
+  "Fk:f1": [
+    "Cs:e0",
+    "Fs:e1",
+    "Fs:e2",
+    "Cs:f0",
+    "Fs:f2",
+    "Cs:g0",
+    "Fs:g1",
+    "Fs:g2"
+  ],
+  "Fk:f2": [
+    "Fs:e1",
+    "Fs:e2",
+    "Fs:e3",
+    "Fs:f1",
+    "Fs:f3",
+    "Fs:g1",
+    "Fs:g2",
+    "Fs:g3"
+  ],
+  "Fk:f3": [
+    "Fs:e2",
+    "Fs:e3",
+    "Fs:e4",
+    "Fs:f2",
+    "Fs:f4",
+    "Fs:g2",
+    "Fs:g3",
+    "Fs:g4"
+  ],
+  "Fk:f4": [
+    "Fs:e3",
+    "Fs:e4",
+    "Bs:e5",
+    "Fs:f3",
+    "Bs:f5",
+    "Fs:g3",
+    "Fs:g4",
+    "Bs:g5"
+  ],
+  "Bk:f5": [
+    "Fs:e4",
+    "Bs:e5",
+    "Fs:e6",
+    "Fs:f4",
+    "Fs:f6",
+    "Fs:g4",
+    "Bs:g5",
+    "Fs:g6"
+  ],
+  "Fk:f6": [
+    "Bs:e5",
+    "Fs:e6",
+    "Fs:e7",
+    "Bs:f5",
+    "Fs:f7",
+    "Bs:g5",
+    "Fs:g6",
+    "Fs:g7"
+  ],
+  "Fk:f7": [
+    "Fs:e6",
+    "Fs:e7",
+    "Fs:e8",
+    "Fs:f6",
+    "Fs:f8",
+    "Fs:g6",
+    "Fs:g7",
+    "Fs:g8"
+  ],
+  "Fk:f8": [
+    "Fs:e7",
+    "Fs:e8",
+    "Fs:e9",
+    "Fs:f7",
+    "Fs:f9",
+    "Fs:g7",
+    "Fs:g8",
+    "Fs:g9"
+  ],
+  "Fk:f9": [
+    "Fs:e8",
+    "Fs:e9",
+    "Cs:e10",
+    "Fs:f8",
+    "Cs:f10",
+    "Fs:g8",
+    "Fs:g9",
+    "Cs:g10"
+  ],
+  "Ck:f10": [
+    "Fs:e9",
+    "Fs:f9",
+    "Fs:g9"
+  ],
+  "Ck:g0": [
+    "Fs:f1",
+    "Fs:g1",
+    "Fs:h1"
+  ],
+  "Fk:g1": [
+    "Cs:f0",
+    "Fs:f1",
+    "Fs:f2",
+    "Cs:g0",
+    "Fs:g2",
+    "Cs:h0",
+    "Fs:h1",
+    "Fs:h2"
+  ],
+  "Fk:g2": [
+    "Fs:f1",
+    "Fs:f2",
+    "Fs:f3",
+    "Fs:g1",
+    "Fs:g3",
+    "Fs:h1",
+    "Fs:h2",
+    "Fs:h3"
+  ],
+  "Fk:g3": [
+    "Fs:f2",
+    "Fs:f3",
+    "Fs:f4",
+    "Fs:g2",
+    "Fs:g4",
+    "Fs:h2",
+    "Fs:h3",
+    "Fs:h4"
+  ],
+  "Fk:g4": [
+    "Fs:f3",
+    "Fs:f4",
+    "Bs:f5",
+    "Fs:g3",
+    "Bs:g5",
+    "Fs:h3",
+    "Fs:h4",
+    "Bs:h5"
+  ],
+  "Bk:g5": [
+    "Fs:f4",
+    "Bs:f5",
+    "Fs:f6",
+    "Fs:g4",
+    "Fs:g6",
+    "Fs:h4",
+    "Bs:h5",
+    "Fs:h6"
+  ],
+  "Fk:g6": [
+    "Bs:f5",
+    "Fs:f6",
+    "Fs:f7",
+    "Bs:g5",
+    "Fs:g7",
+    "Bs:h5",
+    "Fs:h6",
+    "Fs:h7"
+  ],
+  "Fk:g7": [
+    "Fs:f6",
+    "Fs:f7",
+    "Fs:f8",
+    "Fs:g6",
+    "Fs:g8",
+    "Fs:h6",
+    "Fs:h7",
+    "Fs:h8"
+  ],
+  "Fk:g8": [
+    "Fs:f7",
+    "Fs:f8",
+    "Fs:f9",
+    "Fs:g7",
+    "Fs:g9",
+    "Fs:h7",
+    "Fs:h8",
+    "Fs:h9"
+  ],
+  "Fk:g9": [
+    "Fs:f8",
+    "Fs:f9",
+    "Cs:f10",
+    "Fs:g8",
+    "Cs:g10",
+    "Fs:h8",
+    "Fs:h9",
+    "Cs:h10"
+  ],
+  "Ck:g10": [
+    "Fs:f9",
+    "Fs:g9",
+    "Fs:h9"
+  ],
+  "Ck:h0": [
+    "Fs:g1",
+    "Fs:h1"
+  ],
+  "Fk:h1": [
+    "Cs:g0",
+    "Fs:g1",
+    "Fs:g2",
+    "Cs:h0",
+    "Fs:h2",
+    "Cs:i0",
+    "Cs:i1",
+    "Cs:i2"
+  ],
+  "Fk:h2": [
+    "Fs:g1",
+    "Fs:g2",
+    "Fs:g3",
+    "Fs:h1",
+    "Fs:h3",
+    "Cs:i1",
+    "Cs:i2",
+    "Cs:i3"
+  ],
+  "Fk:h3": [
+    "Fs:g2",
+    "Fs:g3",
+    "Fs:g4",
+    "Fs:h2",
+    "Fs:h4",
+    "Cs:i2",
+    "Cs:i3",
+    "Cs:i4"
+  ],
+  "Fk:h4": [
+    "Fs:g3",
+    "Fs:g4",
+    "Bs:g5",
+    "Fs:h3",
+    "Bs:h5",
+    "Cs:i3",
+    "Cs:i4",
+    "Cs:i5"
+  ],
+  "Bk:h5": [
+    "Fs:g4",
+    "Bs:g5",
+    "Fs:g6",
+    "Fs:h4",
+    "Fs:h6",
+    "Cs:i4",
+    "Cs:i5",
+    "Cs:i6"
+  ],
+  "Fk:h6": [
+    "Bs:g5",
+    "Fs:g6",
+    "Fs:g7",
+    "Bs:h5",
+    "Fs:h7",
+    "Cs:i5",
+    "Cs:i6",
+    "Cs:i7"
+  ],
+  "Fk:h7": [
+    "Fs:g6",
+    "Fs:g7",
+    "Fs:g8",
+    "Fs:h6",
+    "Fs:h8",
+    "Cs:i6",
+    "Cs:i7",
+    "Cs:i8"
+  ],
+  "Fk:h8": [
+    "Fs:g7",
+    "Fs:g8",
+    "Fs:g9",
+    "Fs:h7",
+    "Fs:h9",
+    "Cs:i7",
+    "Cs:i8",
+    "Cs:i9"
+  ],
+  "Fk:h9": [
+    "Fs:g8",
+    "Fs:g9",
+    "Cs:g10",
+    "Fs:h8",
+    "Cs:h10",
+    "Cs:i8",
+    "Cs:i9",
+    "Cs:i10"
+  ],
+  "Ck:h10": [
+    "Fs:g9",
+    "Fs:h9"
+  ],
+  "Ck:i0": [
+    "Fs:h1"
+  ],
+  "Ck:i1": [
+    "Fs:h1",
+    "Fs:h2"
+  ],
+  "Ck:i2": [
+    "Fs:h1",
+    "Fs:h2",
+    "Fs:h3"
+  ],
+  "Ck:i3": [
+    "Fs:h2",
+    "Fs:h3",
+    "Fs:h4"
+  ],
+  "Ck:i4": [
+    "Fs:h3",
+    "Fs:h4",
+    "Bs:h5"
+  ],
+  "Ck:i5": [
+    "Fs:h4",
+    "Bs:h5",
+    "Fs:h6"
+  ],
+  "Ck:i6": [
+    "Bs:h5",
+    "Fs:h6",
+    "Fs:h7"
+  ],
+  "Ck:i7": [
+    "Fs:h6",
+    "Fs:h7",
+    "Fs:h8"
+  ],
+  "Ck:i8": [
+    "Fs:h7",
+    "Fs:h8",
+    "Fs:h9"
+  ],
+  "Ck:i9": [
+    "Fs:h8",
+    "Fs:h9"
+  ],
+  "Ck:i10": [
+    "Fs:h9"
+  ]
+};
